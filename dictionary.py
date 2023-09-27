952f4725d2c345ce2f963143767bc8c94fe85311
md. Alamin Faraji <alaminice1617@gmail.com>
2023-09-27 13:23:17 +0600

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,32 @@
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# # print(thisdict["brand"])
+# if "model" in thisdict:
+#   print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict["color"] = "red"
+# print(thisdict)
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(myfamily["child3"]["year"])
